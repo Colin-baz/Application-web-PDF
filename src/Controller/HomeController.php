b309278aefbd0923dbd8fbce9ea11db3db03b3b9
Colin-baz <colin.bazelaire@etudiant.univ-reims.fr>
2025-03-18 00:23:34 +0100

    FixBug : SubscriptionTest

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,11 +9,9 @@ use App\Entity\File;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
     public function index(EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -10,11 +10,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class SubscriptionController extends AbstractController
 {
-    #[Route('/subscription', name: 'subscription_index')]
     public function changeSubscription(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
